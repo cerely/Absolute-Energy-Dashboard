727dfbab3bcba0dc98fe8defc7ce3b2cee10107e
theREALNguyenAnh <ndna9902@gmail.com>
2025-09-16 21:27:55 +0700

    fix up!

--- src/server/routes/conversions.js
+++ src/server/routes/conversions.js
@@ -158,53 +158,55 @@ router.post('/delete', adminAuthMiddleware('delete conversions'), async (req, re
 		type: 'object',
 		required: ['sourceId', 'destinationId', 'meterIds', 'groupIds'],
 		properties: {
-			sourceId: { 
-				type: 'integer', 
-				minimum: 0 
+			sourceId: {
+				type: 'integer',
+				minimum: 0
 			},
-			destinationId: { 
-				type: 'integer', 
-				minimum: 0 
+			destinationId: {
+				type: 'integer',
+				minimum: 0
 			},
-			meterIds: { 
-				type: 'array', 
+			meterIds: {
+				type: 'array',
 				items: { type: 'integer', minimum: 0 },
 				uniqueItems: true,
 				maxItems: 1000
 			},
-			groupIds: { 
-				type: 'array', 
+			groupIds: {
+				type: 'array',
 				items: { type: 'integer', minimum: 0 },
 				uniqueItems: true,
 				maxItems: 1000
 			}
-		}
+		},
+		additionalProperties: false
 	};
 
 	const validatorResult = validate(req.body, validConversion);
 	if (!validatorResult.valid) {
 		log.error(`Got request to delete conversions with invalid conversion data, errors: ${validatorResult.errors}`);
 		failure(res, 400, `Got request to delete conversions with invalid conversion data. Error(s): ${validatorResult.errors}`);
-	}
-	const { sourceId, destinationId, meterIds = [], groupIds = [] } = req.body;
-	const conn = getConnection();
-	try {
-		await conn.tx(async t => {
-			// Update meters if any
-			for (const meterId of meterIds) {
-				await t.none(`UPDATE meters SET default_graphic_unit = NULL WHERE id = ${meterId}`);
-			}
-			// Update groups if any
-			for (const groupId of groupIds) {
-				await t.none(`UPDATE groups SET default_graphic_unit = NULL WHERE id = ${groupId}`);
-			}
-			// Delete conversion
-			await Conversion.delete(sourceId, destinationId, t);
-		});
-		success(res, 'Successfully deleted conversion and updated meters/groups');
-	} catch (err) {
-		log.error(`Error while deleting conversion and updating meters/groups: ${err}`);
-		failure(res, 500, `Error while deleting conversion and updating meters/groups: ${err}`);
+	} else {
+		const { sourceId, destinationId, meterIds = [], groupIds = [] } = req.body;
+		const conn = getConnection();
+		try {
+			await conn.tx(async t => {
+				// Update meters if any
+				for (const meterId of meterIds) {
+					await t.none(`UPDATE meters SET default_graphic_unit = NULL WHERE id = ${meterId}`);
+				}
+				// Update groups if any
+				for (const groupId of groupIds) {
+					await t.none(`UPDATE groups SET default_graphic_unit = NULL WHERE id = ${groupId}`);
+				}
+				// Delete conversion
+				await Conversion.delete(sourceId, destinationId, t);
+			});
+			success(res, 'Successfully deleted conversion and updated meters/groups');
+		} catch (err) {
+			log.error(`Error while deleting conversion and updating meters/groups: ${err}`);
+			failure(res, 500, `Error while deleting conversion and updating meters/groups: ${err}`);
+		}
 	}
 });
 router.post('/simulate-delete', adminAuthMiddleware('simulate deleting conversions'), async (req, res) => {
@@ -214,21 +216,22 @@ router.post('/simulate-delete', adminAuthMiddleware('simulate deleting conversio
 		properties: {
 			sourceId: { type: 'number', minimum: 0 },
 			destinationId: { type: 'number', minimum: 0 }
-		}
+		},
+		additionalProperties: false
 	};
 	const validatorResult = validate(req.body, validConversion);
 	if (!validatorResult.valid) {
 		log.warn(`Got request to simulate deletion of conversions with invalid conversion data, errors: ${validatorResult.errors}`);
 		failure(res, 400, `Got request to delete conversions with invalid conversion data. Error(s): ${validatorResult.errors}`);
-		return;
-	}
-	try {
-		const conn = getConnection();
-		const result = await simulateDeleteConversion(req.body, conn);
-		return res.json(result);
-	} catch (err) {
-		log.error(`Error while simulating deletion of conversion with error(s): ${err}`);
-		failure(res, 500, `Error while simulating deletion of conversion with errors(s): ${err}`);
+	} else {
+		try {
+			const conn = getConnection();
+			const result = await simulateDeleteConversion(req.body, conn);
+			return res.json(result);
+		} catch (err) {
+			log.error(`Error while simulating deletion of conversion with error(s): ${err}`);
+			failure(res, 500, `Error while simulating deletion of conversion with errors(s): ${err}`);
+		}
 	}
 });
 module.exports = router;
